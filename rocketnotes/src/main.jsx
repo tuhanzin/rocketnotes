86e86e26930bd9c8044aaf27f364a050bde65984
Anaildo Silva <anaildolsilva@gmail.com>
2024-09-26 17:40:35 -0300

    feita a configuração global e de theme do app, adicionado componentes como buttons padrões pro app e configurados para modificação de titulo além de configurado a pagina padrão e desabilitação de buttons após cliques. adicionado fontes por css

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from "./Styles/global"
+import theme from './Styles/theme'
+
 import { Details } from './pages/Details'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
     <Details />
+    </ThemeProvider>
   </StrictMode>,
 )
--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,9 +1,13 @@
 import { Container } from "./styles"
+import { Button } from "../../components/button"
 
 export function Details() {
   return(
     <Container>
     <h1>hello world!</h1>
+    <Button title="Entrar" loading/>
+    <Button title="Cadastrar"/>
+    <Button title="Voltar"/>
     </Container>
   )
 }
