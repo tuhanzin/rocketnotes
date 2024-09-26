15831070cf09d6bf0e6214d36e429293caece03a
Anaildo Silva <anaildolsilva@gmail.com>
2024-09-26 16:02:21 -0300

    app transformado em details e passado na main alem de exclusão de outros arquivos assets

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Details } from './pages/Details.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Details />
   </StrictMode>,
 )
---
+++ rocketnotes/src/pages/Details.jsx
@@ -0,0 +1,5 @@
+export function Details() {
+  return(
+    <h1>hello world!</h1>
+  )
+}
